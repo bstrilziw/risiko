c6ba611f3abea9eff7be352b2aa0c932cec8a1d0
Timo Gebauer <t.gebauer@hg-software.de>
2013-11-19 11:12:27 +0100

    unvollständiges Login-System hinzugefügt
    Erste Idee eines Session-basierten Login-Systems.

--- main.rb
+++ main.rb
@@ -8,12 +8,39 @@ configure do
 	Slim::Engine.set_default_options pretty: true, sort_attrs: true
 	set :views, :slim => 'templates'
 	require_relative 'database'
+	enable :sessions
 end
 
 get '/' do
   #bla bla
   slim :home
-end  
+end
+
+post '/' do # TODO: zur Übersichtlichkeit an verschiedene URLs posten? z.B.: '/login'
+	# Session-basiertes Login-System
+	if !session.key?(:account_id) # bereits eingeloggt?
+		if !params[:login_name].nil? && !params[:login_pass].nil?
+			# TODO: Daten auf vollständigkeit prüfen: länge?
+			
+			account = Account.get( :login_name => params[:login_name] )
+			
+			if account.nil? || account.password != params[:login_pass]
+				# Benutzername oder Passwort ungültig
+				@login_info = "Benutzername, oder Passwort ung&uuml;ltig"
+				
+				redirect '/'
+			else
+				# Login-Informationen korrekt
+				session[:account_id] = account.id
+				# TODO: Account-Namen in der Session speichern, oder immer wieder neu aus der DB laden?
+				
+				# TODO: neue Seite anzeigen, nachdem man eingeloggt ist?
+				# 		oder gleiche Seite umgestalten?
+				redirect '/'
+			end
+		end
+	end	
+end
 
 get '/:name' do
   # einfach nur weil ich es kann.
